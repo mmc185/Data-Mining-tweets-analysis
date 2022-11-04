df29c57358c69ec78160d7ddf0d230dc1683787f
Alessandro <capurso89@gmail.com>
2022-11-04 22:52:31 +0100

    move plot_hist function in utils add PreliminarDataUnderstanding.ipynb

--- utilities.py
+++ utilities.py
@@ -47,3 +47,18 @@ def eval_correlation(df_corr, method='pearson'):
     print(correlation_matrix)
     sn.heatmap(correlation_matrix, annot=True, linewidths=.5, ax=ax)
     plt.show()
+
+def plot_hist(dataframe, attribute_name, log=False):
+    df = pd.DataFrame()
+
+    if log:
+        log_attribute_name = attribute_name+'_log'
+        df[log_attribute_name] = np.log(dataframe[attribute_name].values)
+
+        attribute_name = log_attribute_name
+        df[attribute_name] = df[attribute_name].replace(-np.inf, 0)
+
+    else:
+        df[attribute_name] = dataframe[attribute_name].values
+    n_bins = math.ceil(np.log2(len(df[attribute_name])) + 1) #Sturges' rule
+    df.hist(attribute_name, bins = n_bins, log=True)
