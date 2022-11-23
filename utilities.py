7cd2bc7afec243cb22939a12eceadddd82c2ac77
al <a.dipalma9@studenti.unipi.it>
2022-11-23 19:40:45 +0100

    generalize plots in DataUnderstandingPlots

--- utilities.py
+++ utilities.py
@@ -47,9 +47,6 @@ def len_with_int(x):
         return 1
 
 
-
-
-
 # Plot a boxplot w.r.t. a single attribute passed as parameter.
 def plot_boxplot(df, col, log=False, path=None):
     # Plot the distribution of the indicated column
@@ -65,9 +62,9 @@ def plot_boxplot(df, col, log=False, path=None):
         plt.savefig(path)
 
 
-def eval_correlation(df_corr, method='pearson', path=None):
+def eval_correlation(df_corr, method='pearson', path=None, figsize=(10,10)):
     correlation_matrix = df_corr.corr(method=method)
-    fig, ax = plt.subplots(figsize=(10, 10))  # Sample figsize in inches
+    fig, ax = plt.subplots(figsize=figsize)  # Sample figsize in inches
     sn.heatmap(correlation_matrix, annot=True, linewidths=.5, ax=ax)
     if path is not None:
         plt.savefig(path)
@@ -93,18 +90,74 @@ def plot_hist(dataframe, attribute_name, log=False, path=None):
         plt.savefig(path)
 
 
-def date_labels(df, attr, format='M', axes='x', n_ticks = 6):
+def date_labels(df, attr, format='M', axis='x', n_ticks = 6):
     """
     Utility function to put human readable labels for timestamps instead of seconds.
     :param df:
     :param attr:
     :param format:
-    :param axes: 'x' or 'y', the axes where to convert the date labels
+    :param axes: 'x','y', the axes where to convert the date labels
     :return:
     """
     # create indices and labels for the plot. This is done because if you pass the entire sequence it takes years to compute
     indices = [df[attr].min() + q*(df[attr].max()-df[attr].min()) for q in np.array(list(range(0,n_ticks)))/(n_ticks-1)]
-    if axes == 'x':
+
+    if axis == 'x':
         plt.xticks(indices, labels=pd.to_datetime(indices).to_period(format))
-    if axes == 'y':
+    if axis == 'y':
         plt.yticks(indices, labels=pd.to_datetime(indices).to_period(format))
+
+def plot_scatter(df, x_attr, y_attr, path=None, log=False, date_axis=None, xlabel=None, ylabel=None):
+    """
+
+    :param df:
+    :param x_attr:
+    :param y_attr:
+    :param path:
+    :param log:
+    :param date_axis: None, 'x', 'y', or 'both'. If there is some timestamp dimension, pass the respective axis to convert the ticks in human readable format.
+    :return:
+    """
+    plt.scatter(df[x_attr], df[y_attr], color='g', marker='*', )
+
+    if xlabel is None:
+        plt.xlabel(x_attr)
+    else:
+        plt.xlabel(xlabel)
+
+    if ylabel is None:
+        plt.ylabel(y_attr)
+    else:
+        plt.ylabel(ylabel)
+
+    if date_axis is not None:
+        if date_axis == 'both' or date_axis=='x':
+            date_labels(df, x_attr, axis='x')
+        if date_axis == 'both' or date_axis=='y':
+            date_labels(df, y_attr, axis='y')
+
+    if log:
+        plt.yscale('log')
+    plt.legend()
+    if path is None:
+        plt.show()
+    else:
+        plt.savefig(path)
+
+
+def lang_scatter(df, attr, log=False, path=None):
+    plt.figure(figsize=(10, 5))
+    plt.scatter(df[df['bot'] == 0]['lang'],
+                df[df['bot'] == 0][attr], color='g', marker='*', label='Non-bot user')
+    plt.scatter(df[df['bot'] == 1]['lang'],
+                df[df['bot'] == 1][attr], color='r', marker='2', label='Bot user')
+    plt.xlabel('lang')
+    plt.ylabel(attr)
+    if log:
+        plt.yscale('log')
+    plt.legend()
+    if path is None:
+        plt.show()
+    else:
+        plt.savefig(path)
+
