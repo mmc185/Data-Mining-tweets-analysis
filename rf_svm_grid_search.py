6902645fad6521c9377d2dd07675daf39302ce29
al <a.dipalma9@studenti.unipi.it>
2022-12-07 19:15:43 +0100

    create script for remote machine

--- rf_svm_grid_search.py
+++ rf_svm_grid_search.py
@@ -10,13 +10,13 @@ print(f'Number of samples in Test set:', len(ts))
 
 parameters = {
     "n_estimators": range(50,500,100),
-    # "criterion": ["gini","entropy","log_loss"],
-    # "max_depth": [None] + list(range(5,100,10)),
-    # "min_samples_split": [2,4,8,16,32,64],
-    # "min_samples_leaf": [2,4,8,16,32,64,128],
-    # "max_features":["auto","sqrt","log2",None],
-    # "max_leaf_nodes":[None] + list(range(1,10,2)),
-    # "min_impurity_decrease": 0.1 * np.array(range(1,5))
+    "criterion": ["gini","entropy","log_loss"],
+    "max_depth": [None] + list(range(5,100,10)),
+    "min_samples_split": [2,4,8,16,32,64],
+    "min_samples_leaf": [2,4,8,16,32,64,128],
+    "max_features":["auto","sqrt","log2",None],
+    "max_leaf_nodes":[None] + list(range(1,10,2)),
+    "min_impurity_decrease": 0.1 * np.array(range(1,5))
 }
 rf, results_df = grid_search(RandomForestClassifier, parameters, 'random_forest', tr, ts, tr_target, ts_target, n_jobs=40)
 
