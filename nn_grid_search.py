2a6f6aad2f80d3c92ae766b049a5f8205856e3f7
mmc <martina.melerocavallo@outlook.com>
2022-12-19 14:49:11 +0100

    performed part of grid search on nn

--- nn_grid_search.py
+++ nn_grid_search.py
@@ -8,17 +8,17 @@ print(f'Number of samples in Training set:', len(tr))
 print(f'Number of samples in Test set:', len(ts))
 
 parameters = {
-    "hidden_layer_sizes": [[10]], # , [5], [16]],
-    "activation": ["tanh"], #, "tanh"],
+    "hidden_layer_sizes": [[5], [10], [16]],
+    "activation": ["logistic", "tanh"],
     "solver": ["adam"],
-    "alpha": [1e-5], #, 1e-8],
-    "batch_size": [8], #, 16, 32],
-    "learning_rate_init": [1e-2], #, 1e-3, 5e-3],
+    "alpha": [1e-5, 1e-8],
+    "batch_size": [8, 16], #, 32],
+    "learning_rate_init": [1e-2, 1e-3, 5e-3],
     "learning_rate": ['adaptive'],  # 'costant'],
-    "momentum": [0.0]  #, 0.2, 0.4, 0.6, 0.8]
+    "momentum": [0.0, 0.2] #, 0.4, 0.6, 0.8]
 }
 
 #results_df = grid_search(MLPClassifier, parameters, 'nn', tr, tr_target, n_jobs=2)
 #best_classifier = test_best(MLPClassifier, tr, ts, tr_target, ts_target, 'classification/nn/', results_df=results_df)
-grid_search_with_feature_selection(MLPClassifier, parameters, 'nn', tr, ts, tr_target, ts_target, n_jobs=2,
+grid_search_with_feature_selection(MLPClassifier, parameters, 'nn', tr, ts, tr_target, ts_target, n_jobs=-1,
                                        folds=4, n_features=25)
